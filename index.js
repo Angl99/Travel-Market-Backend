cb1f67105ba6370ce7af224ef670d32dccb07d18
Angl99 <anggelplasencia@pursuit.org>
2024-04-16 19:37:58 -0400

    started backend routes and controllers

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const userRouter = require("./Routes/userRoute.js");
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/users", userRouter);
 
 module.exports = app;
