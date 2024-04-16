bd3adef39a6de9d5ebaa11b7143f7b9b6118eee2
lawsonjerry <williambookman@pursuit.org>
2024-04-16 19:55:24 -0400

    added blank vendor router to index

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import cors from 'cors';
 const app = express();
 import userRouter from "./routes/userRoute.js"
 // import prodRouter from "./routes/productRoute.js"
+const vendorRouter = require("./Routes/vendorRouter.js")
 
 app.use(express.json());
 app.use(cors());
 
-// app.use("/users", userRouter);
-// app.use("/products", prodRouter)
+app.use("/users", userRouter);
+app.use("/vendors")
 
 export default app;
