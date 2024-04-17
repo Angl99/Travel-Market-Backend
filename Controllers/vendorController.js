f5efaf87ed3f20e91be7b045a28d5b5f6e506584
lawsonjerry <williambookman@pursuit.org>
2024-04-16 22:07:57 -0400

    added create get by id and update vendors by id

--- Controllers/vendorController.js
+++ Controllers/vendorController.js
@@ -19,11 +19,82 @@ const prisma = new PrismaClient();
       }
     }
  
+
+    //see vendors by id
+
+
+//create vendor
+const createVendor = async (req, res) => {
+    const { userId, shopName, user,  locale_id } = req.body; // Extract data from request body
   
- export { getAllVendors };
+    try {
+      const vendor = await prisma.vendor.create({
+        data: {
+          userId,
+          shopName,
+          user, 
+          locale_id 
+        //   locale: { connect: { id: localeId } }, // Connect to existing locale
+        },
+      });
+      res.json(vendor);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error creating vendor' });
+    }
+  };
+  
 
 
 //see vendors by id
-//create vendor
+
+const getVendorById = async (req, res) => {
+  const vendorId = parseInt(req.params.id); // Extract vendor ID from URL params
+
+  try {
+    const vendor = await prisma.vendor.findUnique({
+      where: { id: vendorId },
+      include: {
+        user: true,
+        locale: true, 
+      },
+    });
+    if (!vendor) {
+      return res.status(404).json({ message: 'Vendor not found' });
+    }
+    res.json(vendor);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching vendor' });
+  }
+};
+
 //update vendor by id
+
+const updateVendorById = async (req, res) => {
+    const vendorId = parseInt(req.params.id); // Extract vendor ID from URL params
+    const { userId, shopName, localeId } = req.body; // Extract update data from request body
+  
+    try {
+      const updatedVendor = await prisma.vendor.update({
+        where: { id: vendorId },
+        data: {
+          userId, // Update fields if provided
+          shopName,
+          locale: { connect: { id: localeId } }, // Update locale if provided
+        },
+      });
+      if (!updatedVendor) {
+        return res.status(404).json({ message: 'Vendor not found' });
+      }
+      res.json(updatedVendor);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error updating vendor' });
+    }
+  };
+  
 //delete vendor by id
+
+
+export { getAllVendors, createVendor, getVendorById, updateVendorById   };
