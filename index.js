6df9f9a848c1816c0df8cb14da285161a982e24c
Angl99 <anggelplasencia@pursuit.org>
2024-04-17 14:30:06 -0400

    implemented user backend routes and controllers

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors';
-const app = express();
-import userRouter from "./routes/userRoute.js"
-// import prodRouter from "./routes/productRoute.js"
+import userRoute from './Routes/userRoute.js';
+import prodRouter from "./routes/productRoute.js"
 
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use("/users", userRouter);
-// app.use("/products", prodRouter)
+app.use("/users", userRoute);
+app.use("/products", prodRouter)
 
 export default app;
